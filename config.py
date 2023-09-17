e601275d21a2896f18077f4308bdbe8f873e8603
ali <ali.naeim.gdrive@gmail.com>
2023-09-17 11:00:01 -0600

    info-type option

--- config.py
+++ config.py
@@ -6,9 +6,10 @@ def parse_args():
     add_arg = parser.add_argument
 
     add_arg("--URL", default="https://www.amazon.com/dp/B08KHR6B3W/", help="the URL of Amazon product webpage", type=str)
+    add_arg("--info-type", default="tabular", choices=["tabular","textual"], help="specify the type of information for ATARI", type=str)
     add_arg("--verbose", action="store_true", help="display the extracted info")
     add_arg("--dump-info", action="store_true", help="dump the extracted info as a json file")
-    add_arg("--dump-info-path", help="take the directory to dump the extracted info", default="./extracted_info")
+    add_arg("--dump-info-path", help="take the directory to dump the extracted info", default="./extracted_info", type=str)
 
     return parser.parse_args()
 
