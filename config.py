c23409b47292af4825552dc25d4af279a3f34060
ali <ali.naeim.gdrive@gmail.com>
2023-09-17 17:06:27 -0600

    add two new options: verbosity-enabled, dump-info-enabled

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import argparse
-from pathlib import Path
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     add_arg = parser.add_argument
-
-    add_arg("--URL", default="https://www.amazon.com/dp/B08KHR6B3W/", help="the URL of Amazon product webpage", type=str)
+    add_arg("--URL", default="https://www.amazon.com/dp/B08KHR6B3W/", help="URL of the Amazon product webpage")
     add_arg("--info-type", default="tabular", choices=["tabular","textual"], help="specify the type of information for ATARI", type=str)
-    add_arg("--verbose", action="store_true", help="display the extracted info")
-    add_arg("--dump-info", action="store_true", help="dump the extracted info as a json file")
+    add_arg("--verbosity-enabled", action="store_true", help="display the extracted info")
+    add_arg("--dump-info-enabled", action="store_true", help="dump the extracted info as a json file")
     add_arg("--dump-info-path", help="take the directory to dump the extracted info", default="./extracted_info", type=str)
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     print(parse_args())
